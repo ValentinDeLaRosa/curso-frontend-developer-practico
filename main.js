2a1a0cf59a6eb2f0cbcc5315aac82e2ad8d449d3
Valentin de la Rosa <vder@unal.edu.co>
2023-07-05 20:55:36 +0000

    Fusion of mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+//Select HTML items
 const navBarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 
-navBarEmail.addEventListener('click', toggleMenu)
+
+navBarEmail.addEventListener('click', toggleMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMenu(){
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
